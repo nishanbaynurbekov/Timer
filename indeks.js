e7cc256a3652b47f78a78575607505248fba8966
Нишанбай <nishanbaynurbekov@gmail.com>
2026-05-30 07:20:52 +0300

    timer-1

---
+++ indeks.js
@@ -0,0 +1,47 @@
+//    function get(){
+//         alert("salam islam")
+//   }
+//  //  setTimeout(get, 3000)
+//  let time = setInterval( get, 1000)
+//   setInterval(() =>{
+//      clearInterval(time)
+//   }, 10000)
+ const timer = document.getElementById("timer")
+ const startBtn = document.getElementById("start")
+ const stopBtn = document.getElementById("stop")
+ const resetBtn = document.getElementById("reset")
+
+ let seconds = "00"
+ let minutes = "00"
+ let hours = "00"
+ let intervalId
+  
+ function updateTimer(){
+     seconds++;
+     timer.innerHTML = ` ${hours}:${minutes}:${seconds}`
+     if(seconds == "05" ) {
+         seconds = "00"
+        minutes++;
+     }
+     else if (minutes == "05") {
+        minutes = "00"
+         hours++
+     } 
+ }
+   function startTimer() {
+     intervalId = setInterval(updateTimer, 1000)
+     startBtn.disabled = true
+      stopBtn.disabled = false
+ }
+ function stopTimer() {
+     clearInterval(intervalId)
+     startBtn.disabled = false
+     stopBtn.disabled = true
+ }
+ function tokto() {
+    timer.innerHTML = clearInterval(updateTimer, timer)
+ }
+ startBtn.onclick = startTimer
+ stopBtn.onclick = stopTimer
+ resetBtn.onclick = tokto
+ 
